edbb79b3b740d5e7bb05378369d2b114260a5d3e
Alberto <carrazco2013@gmail.com>
2023-06-01 11:03:31 -0500

    Styled the day exercise list, now I need to fileter and style the exercise detail page

--- Workouts/urls.py
+++ Workouts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('details/<int:pk>', views.EntryListView.as_view(), name='details'),
     path('delete-exercise/<int:pk>', views.ExerciseDeleteView.as_view(), name='delete-exercise'),
     path('edit-exercise/<int:pk>', views.ExerciseUpdateView.as_view(), name='edit-exercise'),
-    path('entry/<int:pk>', views.EntryListView.as_view()),
+    path('entry/<int:pk>', views.EntryListView.as_view(), name="exercise_detail"),
     path('entry/new', views.EntriesCreateView.as_view(), name="entry_new"),
     path('exercise/new', views.ExercisesCreateView.as_view(), name="exercise.new"),
     path('login', views.LoginInterfaceView.as_view(), name="login"),

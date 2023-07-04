14693ee1de8e56a864a0f0d12b2aedc127024c64
Alberto <carrazco2013@gmail.com>
2023-07-04 14:15:42 -0500

    djhiodhv

--- trackingapp001/settings.py
+++ trackingapp001/settings.py
@@ -145,4 +145,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/workouts/dashboard'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')

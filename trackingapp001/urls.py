85975823a9a2fdf5744709534db16fd91da5786b
Alberto <carrazco2013@gmail.com>
2023-07-04 16:00:59 -0500

    removedworkouts from the URL path patter

--- trackingapp001/urls.py
+++ trackingapp001/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('workouts/', include('Workouts.urls')),
+    path('/', include('Workouts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

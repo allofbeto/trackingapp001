60c28c325fe1fda70b6c91ed7bd90fc7cb314855
Alberto <carrazco2013@gmail.com>
2023-07-05 15:12:06 -0500

    settings.py added a new statfiles_dir path

--- trackingapp001/urls.py
+++ trackingapp001/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -10,6 +10,5 @@ urlpatterns = [
     path('workouts/', include('Workouts.urls')),
 ]
 #
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 

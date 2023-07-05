f0dd035d2011dcf731ac03422e3622727dd19642
Alberto <carrazco2013@gmail.com>
2023-07-05 14:58:21 -0500

    Fixed static file paths on nav, and URL paths

--- trackingapp001/urls.py
+++ trackingapp001/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('workouts/', include('Workouts.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
